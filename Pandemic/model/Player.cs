e7d396f55e381e39b65e12f9f41565758b76a32c
Sanne <sanne.van.mourik@capgemini.com>
2018-08-27 13:20:38 +0200

    Create Player class, including field name, constructor, getter and setter.

---
+++ Pandemic/model/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pandemic.model
+{
+    class Player
+    {
+        private String name;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get => name; set => name = value; }
+
+    }
+}
