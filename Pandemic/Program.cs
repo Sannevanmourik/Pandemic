916e309ff5145f97c89a35cf6f0c9d7c4460f85b
Sanne <sanne.van.mourik@capgemini.com>
2018-08-28 17:35:51 +0200

    Create switch to give user the choice to decrement a disease or not.

--- Pandemic/Program.cs
+++ Pandemic/Program.cs
@@ -18,10 +18,40 @@ namespace Pandemic
             countryChoice.CountryChoice();
 
             //TODO Sanne move this part to a controller
-            SetUpController.GetChosenPlayer(0).TreatDesease();
-            Console.WriteLine("You decremented the amount of diseases in " + SetUpController.GetChosenPlayer(0).currentCountry);
-            Console.WriteLine("Current ammount of diseases: " + SetUpController.GetChosenPlayer(0).currentCountry.AmountOfDiseases);
-            
+            //TODO Sanne make sure player only sees currentCountry name instead of pandemic.....
+
+            Console.WriteLine("Dear user, would you like to decrement the disease by 1? Yes or No? ");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "Yes":
+                    SetUpController.GetChosenPlayer(0).TreatDesease();
+                    Console.WriteLine("You decremented the amount of diseases in " + SetUpController.GetChosenPlayer(0).currentCountry);
+                    Console.WriteLine("Current ammount of diseases: " + SetUpController.GetChosenPlayer(0).currentCountry.AmountOfDiseases);
+                    break;
+
+                case "yes":
+                    SetUpController.GetChosenPlayer(0).TreatDesease();
+                    Console.WriteLine("You decremented the amount of diseases in " + SetUpController.GetChosenPlayer(0).currentCountry);
+                    Console.WriteLine("Current ammount of diseases: " + SetUpController.GetChosenPlayer(0).currentCountry.AmountOfDiseases);
+                    break;
+
+                case "No":
+                    Console.WriteLine("You didn't decrement the amount of diseases in " + SetUpController.GetChosenPlayer(0).currentCountry);
+                    Console.WriteLine("Current ammount of diseases: " + SetUpController.GetChosenPlayer(0).currentCountry.AmountOfDiseases);
+                    break;
+
+                case "no":
+                    Console.WriteLine("You didn't decrement the amount of diseases in " + SetUpController.GetChosenPlayer(0).currentCountry);
+                    Console.WriteLine("Current ammount of diseases: " + SetUpController.GetChosenPlayer(0).currentCountry.AmountOfDiseases);
+                    break;
+
+                default:
+                    break;
+
+
+            }
+
 
             Console.ReadKey();
         }
